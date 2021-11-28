e9eb6ea0ae14293b95d73fec647be0933d6643bf
Kseniia <balerinagizelle@mail.ru>
2021-11-28 11:23:22 +0300

    Add project files.

---
+++ Lab11/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            linearEq linear1 = new linearEq();
+            linear1.k = 0;
+            linear1.b = 0;
+            linear1.Root();
+            Console.ReadKey();
+        }
+
+        struct linearEq
+        {
+            public double k;
+            public double b;
+
+
+
+            public void Root()
+            {
+                {
+                    if (k == 0)
+                    {
+                        if (b == 0)
+                        {
+                            Console.WriteLine("x-любое число");
+                        }
+                        else
+                        { Console.WriteLine("Нет решений"); }
+                    }
+                    Console.WriteLine("{0:0.0}", (-b) / k);
+                }
+
+
+
+            }
+        }
+    }
+
+}   
+
+    
+
+
+
+
+
+
+ 
